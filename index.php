414055896cb30c5c5b6952746eed9e537b7c4e54
Jaelcio De Araujo <jaelcioaraujo.contato@gmail.com>
2022-06-02 09:54:04 -0300

    teste para mysql php

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spring - Site de Teste</title>
+
+    <!-- Importação Boostrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Importação JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Fontes google -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap" rel="stylesheet">
+
+    <!-- Icone rede social -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <div class="container-lg">
+        <div id="imagem">
+            <img src="img/top2.jpg">
+                <article>
+                    <heade>
+                        <h2>A melhor animação já realizada em blender 2.8</h2>
+                        <p>Spring é uma animação 3D criada pelo Blender Studio em 2019.
+                            O curta foi criado com o intuito de demonstrar as novas ferramentas do Blender 2.8, principalmente com relação à visualização da animação em tempo de renderização (Blender Eevee).
+                            No "filminho" é possivel ver o poder dessa ferramente incrivel! A equipe Blender mantem esse trabalho constantemente, ou seja, lançar uma animação, sempre surge uma nova versão do software.
+                        </p>
+
+                        <iframe width="1280" height="540" src="https://www.youtube.com/embed/WhWc3b3KhnY" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        
+                        <p>
+                            Conheça o site da Blender Studio <a href="https://www.blender.org" target="_blank">aqui</a><br/>
+                        </p>
+                        <p> O Blender tem mostrado que software livre pode alcançar um nivel de excelência e continuar sendo uma das ferramentas de desenvolvimento 3D mais poderosas e populares do mundo.</p>
+                        <div style="width: 100px; margin: 0 auto;">
+                        <p>Redes Sociais</p>
+                        <a href="https://pt-br.facebook.com/YourOwn3DSoftware/" class="fa fa-facebook" style="font-size: 30px; color:aliceblue;"></a>
+                        <a href="https://twitter.com/blender" class="fa fa-twitter" style="font-size: 30px; color:aliceblue;"></a>
+                        <a href="https://www.youtube.com/channel/UCSMOQeBJ2RAnuFungnQOxLg" class="fa fa-youtube" style="font-size: 30px; color:aliceblue;"></a>
+                        </div> 
+                        
+                    </heade>
+                </article>
+        </div>
+    </div>
+</body>
+</html>
